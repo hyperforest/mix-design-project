c97b0c41e49c9ce3716c07ea2afbafdc4f6727e7
hyperforest <rimbaerlangga@gmail.com>
2024-04-16 11:07:04 +0700

    update train script

--- src/train.sh
+++ src/train.sh
@@ -1,5 +1,4 @@
-# python train.py --config_path ../configs/config_v6.json --algo sgd --n_trials 1 --log_model false
-python train.py --config_path ../configs/config_v6.json --algo xgblr --n_trials 100 --log_model true
+python train.py --config_path ../configs/config_v7.json --algo xgblr --n_trials 100 --log_model true
 
 # python train.py --config_path ../configs/config_v5.json --algo rf --n_trials 100 --log_model true
 # python train.py --config_path ../configs/config_v5.json --algo lr --n_trials 20 --log_model true
@@ -7,3 +6,5 @@ python train.py --config_path ../configs/config_v6.json --algo xgblr --n_trials 
 # python train.py --config_path ../configs/config_v5.json --algo svm --n_trials 50
 # python train.py --config_path ../configs/config_v5.json --algo nn --n_trials 30
 # python train.py --config_path ../configs/config_v5.json --algo sgd --n_trials 20
+# python train.py --config_path ../configs/config_v6.json --algo sgd --n_trials 1 --log_model false
+# python train.py --config_path ../configs/config_v6.json --algo xgblr --n_trials 100 --log_model true
